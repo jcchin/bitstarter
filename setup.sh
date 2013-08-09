459bce9268d4d5b3a0def556099e592c3a582007
jcchin <jchin89@gmail.com>
2013-08-09 00:40:02 -0400

    split up setup (exit in between)

--- setup.sh
+++ setup.sh
@@ -71,37 +71,3 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
-#--------------------------setup--ssjs.sh----------------
-
-# Install node packages
-npm install
-
-echo -e "\n\nNOW ENTER YOUR HEROKU PASSWORD"
-# Set up heroku.
-#  - https://devcenter.heroku.com/articles/config-vars#setting-up-config-vars-for-a-deployed-application
-#  - https://devcenter.heroku.com/articles/heroku-postgresql#provisioning-the-add-on
-heroku login
-#heroku create
-echo -e "\n\nPress ENTER A FEW TIMES"
-ssh-keygen -t rsa -C "jchin89@gmail.com" #press enter a few times
-heroku keys:add
-heroku addons:add heroku-postgresql:dev
-heroku pg:promote `heroku config  | grep HEROKU_POSTGRESQL | cut -f1 -d':'`
-heroku plugins:install git://github.com/ddollar/heroku-config.git
-
-echo -e "\n\nCOPY THE OUTPUT TO GITHUB\n"
-cat ~/.ssh/id_rsa.pub  #copy the output
-
-# Set up heroku configuration variables
-# https://devcenter.heroku.com/articles/config-vars
-# IMPORTANT: 
-#  - Edit .env to include your own COINBASE_API_KEY and HEROKU_POSTGRES_URL.
-#  - Modify the .env.dummy file, and DO NOT check .env into the git repository.
-#  - See .env.dummy for details.
-# cp .env.dummy .env
-
-
-
-# For local: setup postgres (one-time) and then run the local server
-#-----------pgsetup.sh
-./pgsetup.sh
